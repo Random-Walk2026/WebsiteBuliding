c2f97fbe80475512962000d3178e5c418bdcfc98
Random-Walk2026 <258099051+Random-Walk2026@users.noreply.github.com>
2026-04-25 06:08:27 +0800

    Update HEO_HERO_COVER_TITLE to English

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   HEO_HERO_TITLE_5: 'NotionNext 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://www.lamjinlab.com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Just browsing',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
